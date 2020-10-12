99fb8de14f40b6d437939c44bc07a88f97cf5c5a
ITJoker233 <649258008@qq.com>
2020-10-12 20:16:15 +0800

    Update Some Data...

--- Butter/Image.py
+++ Butter/Image.py
@@ -1,1 +1,21 @@
 import cv2
+
+class Image:
+    
+    def __init__(self,filepath):
+        self.filepath = filepath
+
+    def read(self):
+        pass
+
+    def save(self):
+        pass
+
+    def binarization(self):
+        pass
+
+    def gaussianBlur(self):
+        pass
+
+    def commic(self):
+        pass
