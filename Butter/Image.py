3a648021ef4ada718b3e43027f99f90f705384fd
ITJoker233 <649258008@qq.com>
2021-07-06 13:25:30 +0800

    🍕 Update Image.py , .gitignore

--- Butter/Image.py
+++ Butter/Image.py
@@ -1,15 +1,20 @@
-import cv2
+import cv2 as cv
 
 class Image:
     
-    def __init__(self,filepath):
-        self.filepath = filepath
-
-    def read(self):
-        pass
-
-    def save(self):
-        pass
+    def __init__(self,imgpath):
+        self.imgpath = imgpath
+        self.image = cv.imread(self.imgpath)
+        
+    def read(self,imgpath,**kw):
+        self.image = cv.imread(imgpath,**kw)
+        return self.image
+    
+    def show(self,**kw):
+        cv.imshow(self.image,**kw)    
+    
+    def save(self,filename,**kw):
+        cv.imwrite(filename=filename,img=self.image,**kw)
 
     def binarization(self):
         pass
